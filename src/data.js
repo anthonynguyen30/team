b18f3401308d8258d135baca966e503df603e54f
anthonynguyen30 <94013799+anthonynguyen30@users.noreply.github.com>
2022-01-10 20:47:26 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://ca.slack-edge.com/E27SFGS2W-W4XCLKZQE-1a2e93a89bbc-512',
+    name: 'Justin McCoy',
+    title: 'HCBT Americas Manager',
+    quote:
+      'If you quit ONCE it becomes a habit.Never quit!!!',
+  },
+  {
+    id: 2,
+    image:
+      'https://ca.slack-edge.com/E27SFGS2W-W0121PWESN5-cc598227cfc8-512',
+    name: 'Arif',
+    title: 'Architect ?',
+    quote:
+      'Talent wins games, but teamwork and intelligence wins championships.',
+  },
+  {
+    id: 3,
+    image:
+      'https://ca.slack-edge.com/E27SFGS2W-W4EBR2Q4U-d7eaa058192f-512',
+    name: 'Marek',
+    title: 'Architect ?',
+    quote:
+      'You must expect great things of yourself before you can do them.',
+  },
+  {
+    id: 4,
+    image:
+      'https://ca.slack-edge.com/E27SFGS2W-W3N115UV8-19c37b69b6da-512',
+    name: 'JeanCarl',
+    title: 'Architect',
+    quote:
+      'Never say never, because limits, like fears, are often just an illusion.',
+  },
+];
+
+export default people;
